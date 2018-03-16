701da3ee4863c443a433b6adac81df95a627ac6d
derlolard <derlolard@gmail.com>
2018-03-16 19:35:44 +0400

    [*] my-team grid into own component

---
+++ client/src/app/routes/my-team/team-grid/info-popup/InfoPopup.tsx
@@ -0,0 +1,20 @@
+
+
+
+export const InfoPopup = () => (
+	<div className='my-team-grid-info-popover' >
+
+		<div className='my-team-grid-info-popover__nick-title'>Нікнейм</div>
+		<div className='my-team-grid-info-popover__nick' >112233</div>
+
+		<div className='my-team-grid-info-popover__sub-title' >Мобільний телефон:</div>
+		<div className='my-team-grid-info-popover__sub-value' >+38 (093) 888-88-88</div>
+
+		<div className='my-team-grid-info-popover__sub-title' >E-mail:</div>
+		<div className='my-team-grid-info-popover__sub-value' >mail.mail@com</div>
+
+		<div className='my-team-grid-info-popover__sub-title' >Інвестиція:</div>
+		<div className='my-team-grid-info-popover__sub-value' >mail.mail@com</div>
+
+	</div>
+);
