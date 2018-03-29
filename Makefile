03a47564eaa40b20237135e5000be1e0855cae29
Delphit <mykola.mykhailyshyn@gmail.com>
2018-03-29 19:29:34 +0200

    Added: Docker

---
+++ Makefile
@@ -0,0 +1,20 @@
+# vars
+prod-dockerfile = -f docker-compose.yml
+
+# Production env
+
+.PHONY: build-prod
+build-prod:
+	docker-compose $(prod-dockerfile) build
+	$(MAKE) install-backend-dependencies
+
+.PHONY: prod
+prod:
+	docker-compose down
+	docker-compose $(prod-dockerfile) up -d
+
+# helpers
+
+.PHONY: install-backend-dependencies
+install-backend-dependencies:
+	docker-compose run --rm --no-deps backend npm install
--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { RouterModule, Routes } from "nest-router";
+import { RouterModule, Routes } from 'nest-router';
 
 import { AccountModule } from './modules/account/account.module';
 import { InvestmentModule } from './modules/investment/investment.module';
-import { AdminModule } from "./modules/admin/admin.module";
-import { ShoppingRequestsModule } from "./modules/admin/modules/shopping-requests/shopping-requests.module";
-import { UsersListModule } from "./modules/admin/modules/users/users.module";
+import { AdminModule } from './modules/admin/admin.module';
+import { ShoppingRequestsModule } from './modules/admin/modules/shopping-requests/shopping-requests.module';
+import { UsersListModule } from './modules/admin/modules/users/users.module';
 import { MONGODB_URI } from './config/environments.config';
-import { User } from "./entity/user.entity";
-import { MinerModule } from "./modules/miner/miner.module";
-
+import { User } from './entity/user.entity';
+import { MinerModule } from './modules/miner/miner.module';
 
 const adminRoutes: Routes = [
   {
@@ -40,7 +39,7 @@ const adminRoutes: Routes = [
     AdminModule,
     ShoppingRequestsModule,
     UsersListModule,
-    MinerModule
+    MinerModule,
   ],
 })
 export class ApplicationModule {}
