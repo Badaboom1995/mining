383cd3737bf03ce2319b0b221fc999f7bdc0f5bd
getktsa <getktsa@gmail.com>
2018-04-03 22:56:05 +0400

    remove unused links, api path for dev

--- client/src/app/constants.ts
+++ client/src/app/constants.ts
@@ -1,4 +1,4 @@
 
 
 
-export const API_URL = DEV ? LOCAL ? 'http://localhost:3000/api' : 'http://theminingrigs.com' : '/api';
+export const API_URL = DEV ? LOCAL ? 'http://localhost:3000/api' : 'http://52.25.161.162/api' : '/api';
