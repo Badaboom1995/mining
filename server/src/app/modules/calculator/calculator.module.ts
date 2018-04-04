b0db1e1bc046af9fa8c3b9100a69391b5310fe2a
Delphit <mykola.mykhailyshyn@gmail.com>
2018-04-04 15:31:18 +0200

    Added: calculate route for calculating mining profit

--- server/src/app/modules/calculator/calculator.module.ts
+++ server/src/app/modules/calculator/calculator.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CalculatorController } from './controllers/calculator.controller';
 import { CalculatorService } from './services/calculator.service';
+import { MinerTypesService } from "../admin/modules/miner-types/services";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { MinerType } from "../../entity/miner-type.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MinerType])],
 	controllers: [CalculatorController],
-	components: [CalculatorService]
+	components: [CalculatorService, MinerTypesService]
 })
-export class CalculatorModule {
-
-}
+export class CalculatorModule {}
